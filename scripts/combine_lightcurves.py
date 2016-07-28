f3a6aa006d228d7b29dfc84e9317107f7be07baf
Kate Storey-Fisher <ksf@google.com>
2016-07-28 20:47:16 +0000

    updated notification listener

--- scripts/combine_lightcurves.py
+++ scripts/combine_lightcurves.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import json
 import argparse
-from json.decoder import JSONDecodeError
+#from json.decoder import JSONDecodeError
 
 from pocs.utils.google.storage import PanStorage
 
--- scripts/notification_listener.py
+++ scripts/notification_listener.py
@@ -12,17 +12,19 @@ def receive():
 		'status_code': 200
 	}
 	pic = "PIC_J0326137+295015"
-	message_str = request.get_data()
+	message_str = request.get_data().decode("utf-8")
 	message = json.loads(message_str)
 	object_name = message['object_name']
 	bucket_name = message['bucket_name']
-	#combine_lightcurves.write_output("filename.json", "data")
+	print(object_name)
+	combine_lightcurves.build_mlc(pic)
 	#exec('combine_lightcurves.py')
 	return jsonify(response), 200
 
 if __name__ == '__main__':
 	app.run(
-		"0.0.0.0",
-		8080,
-		True
+		host="0.0.0.0",
+		port=8080,
+		debug=True,
+		threaded=True
 	)

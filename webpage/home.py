857d1815b2063e9c75cb8ded553e1e3ea55f2a2c
Kate Storey-Fisher <ksf@google.com>
2016-08-31 17:43:26 -0700

    build basic webpage to display light curves, for demo

--- webpage/home.py
+++ webpage/home.py
@@ -1,13 +1,19 @@
 import webapp2
-
+import os
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, webapp2 World!')
+        pic = 'PIC_J18365633+3847012'
+        image_name = '{}.png'.format(pic)
+        caption = "PANOPTES Light Curve Dashboard"
+        probability = "There is a XXX probability that an exoplanet is orbiting star {}.".format(pic)
+        template_values = {'caption': caption, "probability": probability, 'image_name': image_name}
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
 
 
-APP = webapp2.WSGIApplication([
+app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)

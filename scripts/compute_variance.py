053562768a2d984772958c6c6a75d4f001e7e2ff
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-03-02 11:47:13 +1100

    * Fixing comparison script

--- scripts/compute_variance.py
+++ scripts/compute_variance.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import pandas as pd
-
 from astropy.time import Time
 
 from piaa.observation import Observation
@@ -30,21 +28,14 @@ if __name__ == '__main__':
 
     # Normalize first
     print("Normalizing stamps")
-    obs.create_normalized_stamps()
-    print("Normalization done: {:02f} seconds".format(((Time.now() - start).sec)))
+    obs.create_subtracted_stamps()
+    normal_done = Time.now()
+    print("Normalization done: {:02f} seconds".format(((normal_done - start).sec)))
 
     print("Getting variance for index {}".format(args.target_index))
     print(obs.point_sources.iloc[args.target_index])
-    result = obs.get_variance_for_target(args.target_index)
 
-    obs.point_sources['V'] = pd.Series(result)
+    obs.get_variance_for_target(args.target_index)
+    print("Variance done: {:02f} seconds".format(((Time.now() - normal_done).sec)))
 
-    # Sort the values by lowest total variance
-    obs.point_sources.sort_values(by=['V'], inplace=True)
-
-    # Save values to file
-    output_file = 'vary_{}.csv'.format(args.target_index)
-    print("Saving to file: {}".format(output_file))
-    obs.point_sources.to_csv(output_file)
-
-    print("Processing time: {:02f} seconds".format(((Time.now() - start).sec)))
+    print("Total time: {:02f} seconds".format(((Time.now() - start).sec)))

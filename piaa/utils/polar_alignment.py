b4a46aeaf25ff527e3a43aba7453de7d9b8d19d9
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-11-12 09:04:36 +1100

    Polar alignment can be in any direction

--- piaa/utils/polar_alignment.py
+++ piaa/utils/polar_alignment.py
@@ -71,7 +71,7 @@ def plot_center(pole_fn, rotate_fn, pole_center, rotate_center):
     ax.scatter(pole_cx, pole_cy, color='g', marker='x', lw=5)
     ax.imshow(d0 + d1, cmap='Greys_r', norm=norm, origin='lower')
 
-    if (pole_cy - rotate_cy) > 50:
+    if np.abs(pole_cy - rotate_cy) > 50:
         ax.arrow(rotate_cx, rotate_cy, pole_cx - rotate_cx, pole_cy -
                  rotate_cy, fc='r', ec='r', width=20, length_includes_head=True)
 

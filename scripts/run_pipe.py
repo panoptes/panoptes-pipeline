c63312f7bf9593177bd896041c3d4a324fb45e52
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-02-25 18:50:43 +1100

    Dumping changes

--- scripts/run_pipe.py
+++ scripts/run_pipe.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import os
+import argparse
 import numpy as np
 import concurrent.futures
+import h5py
+from glob import glob
 from itertools import zip_longest
 from getpass import getpass
 
@@ -11,7 +14,7 @@ from astropy.wcs import WCS
 from astropy.nddata.utils import Cutout2D, NoOverlapError, PartialOverlapError
 from photutils import make_source_mask
 
-from piaa.utils.helpers import get_observation_blobs, unpack_blob, get_stars
+from piaa.utils.helpers import get_stars
 from piaa import pipeline
 
 from pocs.utils import current_time
@@ -44,12 +47,12 @@ def calibrate_image(input_args):
     image_path = os.path.join(working_dir, image_id + '.npy')
     _print("Starting", image_id)
 
-    # if not os.path.exists(image_path):
-    d1 = pipe.run(d0, source_mask=source_mask)
-    #np.save(image_path, d1.filled(0).astype(np.int16))
+    if not os.path.exists(image_path):
+        d1 = pipe.run(d0, source_mask=source_mask)
+        np.save(image_path, d1.filled(0).astype(np.int16))
 
     _print("Done", image_id)
-    return d1
+    return image_path
 
 
 def extract_stamp(input_args):
@@ -74,7 +77,7 @@ def extract_stamp(input_args):
     return stamp.flatten()
 
 
-def main(seq_id):
+def main(seq_id=None, image_dir=None):
     global source_mask
     _print("Begin sequence", seq_id)
 
@@ -85,11 +88,7 @@ def main(seq_id):
     os.makedirs(working_dir, exist_ok=True)
     os.makedirs(psc_dir, exist_ok=True)
 
-    # Get blobs from storage
-    blobs = get_observation_blobs(seq_id)
-
-    _print("Unpacking blobs")
-    fits_files = [unpack_blob(b) for b in blobs]
+    fits_files = sorted(glob('*.fz'.format(image_dir)))
 
     _print("Making source mask")
     with fits.open(fits_files[0]) as hdu:
@@ -97,12 +96,26 @@ def main(seq_id):
             hdu[0].data, snr=3, npixels=3, sigclip_sigma=3, sigclip_iters=5, dilate_size=12)
         wcs = WCS(hdu[0].header)
 
-    _print("Calibrating data")
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-        calibrate_args = list(zip_longest(fits_files, [], fillvalue=working_dir))
-        #calibrated_list = list(executor.map(calibrate_image, calibrate_args))
-        np.savez_compressed(os.path.join(working_dir, 'cube'),
-                            list(executor.map(calibrate_image, calibrate_args)))
+    hdf5_path = os.path.join(working_dir, image_dir + '.hdf5')
+
+    if not os.path.exists(hdf5_path):
+        _print("Calibrating data")
+        with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+            calibrate_args = list(zip_longest(fits_files, [], fillvalue=working_dir))
+            calibrated_list = list(executor.map(calibrate_image, calibrate_args))
+
+        _print("Create empty HDF5 data cube")
+        hdf5 = h5py.File(hdf5_path)
+        cube_dset = hdf5.create_dataset(
+            'cube', (len(fits_files), source_mask.shape[0], source_mask.shape[1]))
+        for i, f in enumerate(fits_files):
+            cube_dset[i] = fits.getdata(f)
+
+        _print("Saving calibrated data to HDF5 cube")
+        for i, f in enumerate(calibrated_list):
+            d0 = np.load(f)
+            cube_dset[i] = d0
+            os.remove(f)
 
     _print("Looking up stars")
     wcs_footprint = wcs.calc_footprint()
@@ -116,55 +129,56 @@ def main(seq_id):
     _print("Getting all WCS")
     wcs_list = [WCS(f) for f in fits_files]
 
-    stamp_size = 7
+# stamp_size = 7
 
-    bad_positions = list()
-#    _print("Looping files to extract stars")
-#    for f_idx, f in enumerate(calibrated_list):
-#        _print("Extracting from", f)
-#        # Get star positions for this file
-#        star_positions = np.array(wcs_list[f_idx].wcs_world2pix(star_table['ra'], star_table['dec'], 0)).T
-#
-#        d0 = np.load(f, mmap_mode='r')
-#
-#        # Get stamp for each star
-#        for pos_idx,  position in enumerate(star_positions):
-#            if np.any(position < 0) or pos_idx in bad_positions:
-#                continue
-#
-#            psc_path = os.path.join(working_dir, 'psc', '{}.npy'.format(star_table[pos_idx]['id']))
-#
-#            try:
-#                stamp = Cutout2D(d0, position, stamp_size, mode='strict').data.flatten()
-#            except (NoOverlapError, PartialOverlapError):
-#                bad_positions.append(pos_idx)
-#            else:
-#                # Load existing cube, append, save
-#                try:
-#                    # Load existing cube
-#                    psc = np.load(psc_path, mmap_mode='r+')
-#                except FileNotFoundError:
-#                    # Create a cube of zeros
-#                    np.save(psc_path, np.zeros((len(calibrated_list), stamp_size*stamp_size)))
-#                    # Load new cube
-#                    psc = np.load(psc_path, mmap_mode='r+')
-#                finally:
-#                    psc[f_idx] = stamp
-#                    psc.flush()
+#  bad_positions = list()
+# _print("Looping files to extract stars")
+# for f_idx, f in enumerate(calibrated_list):
+#     _print("Extracting from", f)
+#     # Get star positions for this file
+#     star_positions = np.array(wcs_list[f_idx].wcs_world2pix(star_table['ra'], star_table['dec'], 0)).T
 
-    #_print("Getting star positions")
+#     d0 = np.load(f, mmap_mode='r')
+
+#     # Get stamp for each star
+#     for pos_idx,  position in enumerate(star_positions):
+#         if np.any(position < 0) or pos_idx in bad_positions:
+#             continue
+
+#         psc_path = os.path.join(working_dir, 'psc', '{}.npy'.format(star_table[pos_idx]['id']))
+
+#         try:
+#             stamp = Cutout2D(d0, position, stamp_size, mode='strict').data.flatten()
+#         except (NoOverlapError, PartialOverlapError):
+#             bad_positions.append(pos_idx)
+#         else:
+#             # Load existing cube, append, save
+#             try:
+#                 # Load existing cube
+#                 psc = np.load(psc_path, mmap_mode='r+')
+#             except FileNotFoundError:
+#                 # Create a cube of zeros
+#                 np.save(psc_path, np.zeros((len(calibrated_list), stamp_size*stamp_size)))
+#                 # Load new cube
+#                 psc = np.load(psc_path, mmap_mode='r+')
+#             finally:
+#                 psc[f_idx] = stamp
+#                 psc.flush()
+
+    _print("Getting star positions")
+
     # For each WCS, get the X,Y position and then add the star ID to same array
-#    star_positions = [
-#            np.insert(
-#                np.array(w.wcs_world2pix(star_table['ra'], star_table['dec'], 0)).T,
-#                2,
-#                star_table['id'],
-#                axis=1
-#            )
-#        for w in wcs_list
-#    ]
-#
-#    np.save(os.path.join(working_dir,'star_positions'), star_positions)
+    star_positions = [
+        np.insert(
+            np.array(w.wcs_world2pix(star_table['ra'], star_table['dec'], 0)).T,
+            2,
+            star_table['id'],
+            axis=1
+        )
+        for w in wcs_list
+    ]
+
+    np.save(os.path.join(working_dir, 'star_positions'), star_positions)
 
 
 #    _print("Extracting stamps from stars (size={})".format(stamp_size))
@@ -200,7 +214,16 @@ def main(seq_id):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image-dir', required=True, type=str,
+                        default='/var/panoptes/images',
+                        help="Image directory containing FITS files")
+    parser.add_argument('--seq_id', required=True, type=str,
+                        help="Image sequence of Observation")
+
     if 'PGPASSWORD' not in os.environ:
         os.environ['PGPASSWORD'] = getpass(prompt='Catalog DB Password: ')
-    seq_id = 'PAN001/HatP20/14d3bd/20180202T061353'
-    main(seq_id)
+
+    args = parser.parse_args()
+
+    main(**args)

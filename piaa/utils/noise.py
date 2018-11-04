93410ea7d0cada86389c5470bf6a5ce60bb1e2ba
Wilfred Gee <wtylergee@gmail.com>
2018-11-04 21:20:15 +0000

    Cleaing up logs and removing test from photon count

--- piaa/utils/noise.py
+++ piaa/utils/noise.py
@@ -23,43 +23,36 @@ def estimated_photon_count(
     flux_params = helpers.get_photon_flux_params(filter_name)
     logger.info(f'Using flux params: {flux_params}')
     
-    flux0 = flux_params['flux0'] * u.jansky
-    photon0 = flux_params['photon0'] * u.photon #/ (u.cm * u.cm) / (u.angstrom)
+    flux0 = flux_params['flux0'] #* u.jansky
     extinction = flux_params['extinction']
     filter_center = flux_params['lambda_c'] * u.micron
     dlambda_ratio = flux_params['dlambda_ratio']
     filter_width = flux_params['filter_width'] * u.nm
 
-    logger.info(f'Initial photons: {photon0:.02f}')
+    logger.info(f'Initial flux: {flux0:.02f} J')
 
     # Adjust for magnitude (scales magnitude)
-    photon0 *= 10**(-0.4 * magnitude)
-    logger.info(f'Magnitude scaled photons: {photon0:.02f}')
+    flux0 = 10**(-0.4 * magnitude)  * flux0
+    logger.info(f'Magnitude scaled ({filter_name}={magnitude}) flux: {flux0:.02f} J')
+    flux0 *= 1.51e7 * dlambda_ratio * aperture_area.to(u.m**2)
+    flux0 = flux0.value
+    logger.info(f'Magnitude scaled flux: {flux0:.02f} photons')
 
     # Get initial instrumental magnitude
-    imag0 = -2.5 * np.log10(photon0.value)
-    logger.info(f'Inital inst mag: {imag0:.02f}')
+    imag0 = -2.5 * np.log10(flux0)
+    logger.info(f'Initial inst mag flux: {imag0:.02f}')
 
     # Atmosphere causes flux reduction (adds magnitude)
     imag0 += extinction * airmass
-    logger.info(f'Atmo corrected inst mag: {imag0:.02f}')
+    logger.info(f'Airmass corrected (X={airmass:.02f}) inst mag: {imag0:.02f}')
 
     # Convert back to photons
-    photon1 = 10**(imag0 / -2.5) / (u.cm * u.cm) / (u.angstrom)
+    photon1 = 10**(imag0 / -2.5) #/ (u.cm * u.cm) / (u.angstrom)
     logger.info(f'Corrected photons: {photon1:.02f}')
-
-    # Multiply by the filter width (which photons get through)
-    photon1 *= filter_width.to(u.angstrom)
-    logger.info(f'Filter width photons: {photon1:.02f}')
-
-    # Multiply by collecting area (collect more photons)
-    logger.info(f'Aperture area: {aperture_area:0.4f}')
-    photon1 *= aperture_area.to(u.cm * u.cm)
-    logger.info(f'Area collected photons: {photon1:.02f}')
 
     # Quantum efficiency of detector (limit what is detected)
     photon1 *= qe
-    logger.info(f'QE photons: {photon1:.02f}')
+    logger.info(f'QE ({qe:.0%}) photons: {photon1:.02f}')
          
     return photon1
 

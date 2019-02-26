e420dc3551b2fda5b496fa794365e90301b13b24
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-02-27 08:30:52 +1100

    Update to metadata db schema
    * `images` table adds a `headers` column of type `jsonb` that will
    store the FITS headers.
    * `images` table simplified.
    * Column name consistency

--- scripts/pan-meta-db.sql
+++ scripts/pan-meta-db.sql
@@ -5,7 +5,7 @@ CREATE TABLE units (
 	lat numeric(6,2),
 	elevation numeric(7,2),
 	commission_date date,
-	active boolean DEFAULT false
+	active boolean DEFAULT true
 );
 
 CREATE TABLE cameras (
@@ -20,8 +20,7 @@ CREATE TABLE sequences (
 	unit_id integer REFERENCES units (id) ON DELETE RESTRICT NOT NULL,
 	start_date timestamp UNIQUE,
   	coord_bounds box,
-  	exp_time numeric(6,2), 
-  	ra_rate numeric(3,2),  
+  	exptime numeric(6,2), 
   	pocs_version varchar(15),
   	piaa_state varchar(45) DEFAULT 'initial'
 
@@ -33,43 +32,32 @@ CREATE TABLE images (
 	id char(29) PRIMARY KEY,
 	sequence_id char(29) REFERENCES sequences (id) ON DELETE RESTRICT NOT NULL,
 	camera_id char(6) REFERENCES cameras (id) ON DELETE RESTRICT NOT NULL,
-	date_obs timestamp,
+	obstime timestamp,
 	center_ra numeric(7,4),
 	center_dec numeric(7,4),
-	moon_fraction numeric(4,3),
-	moon_separation numeric(6,3),
 	ra_mnt numeric(7,4), 
 	ha_mnt numeric(7,4),
 	dec_mnt numeric(7,4), 
-	airmass numeric(4,3),
-	exp_time numeric(6,2),
-	iso int,
-	cam_temp numeric(4,2), 
-	cam_circconf numeric(4,3),
-	cam_colortmp numeric(4,0),
-  	cam_measrggb varchar(20),
-  	cam_measured_ev numeric(4,2),
-	cam_measured_ev2 numeric(4,2),
-  	cam_white_level_n int,
-  	cam_white_level_s int,
-	cam_red_balance numeric(7,6),
-	cam_blue_balance numeric(7,6),
+	exptime numeric(6,2),
+	headers jsonb,
   	file_path text
 );
 CREATE INDEX on images (file_path);
 CREATE INDEX on images (center_ra);
 CREATE INDEX on images (center_dec);
+CREATE INDEX on images (obstime);
 
 CREATE TABLE stamps (
-	image_id char(29), -- REFERENCES images (id) ON DELETE RESTRICT NOT NULL,
-	pic_id bigint,
-	date_obs timestamp,
+	image_id char(29) REFERENCES images (id) NOT NULL,
+	picid bigint NOT NULL,
+	obstime timestamp,
 	ra numeric(7,4), 
 	dec numeric(7,4),	
-	original_position integer ARRAY,
+	x_pos numeric(7,4),	
+	y_pos numeric(7,4),	
 	data float ARRAY,
-	PRIMARY KEY (image_id, pic_id)
+	PRIMARY KEY (picid, image_id)
 );
-CREATE INDEX on stamps (pic_id);
+CREATE INDEX on stamps (picid);
 CREATE INDEX on stamps (ra);
 CREATE INDEX on stamps (dec);

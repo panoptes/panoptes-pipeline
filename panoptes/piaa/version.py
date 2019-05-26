3534e11c3ce42bc1beb373cf553dce03b7df278b
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-05-26 11:46:15 +1000

    Make proper packer (#44)
    * **Namespace change**
    * Docker updates
    * Add pypi deploy to travis
    * Updating requirements, using only setuptools

---
+++ panoptes/piaa/version.py
@@ -0,0 +1,6 @@
+# this file was automatically generated
+major = 0
+minor = 0
+release = 1
+
+__version__ = '{}.{}.{}'.format(major, minor, release)
--- version.py
+++
@@ -1,6 +0,0 @@
-# this file was automatically generated
-major = 0
-minor = 2
-release = 1
-
-version = '{}.{}.{}'.format(major, minor, release)

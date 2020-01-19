9d56302ebc0eb9a8317cebcb5cf9d92b4b12f125
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-01-19 07:23:10 -1000

    WCS footprint improvements (#61)
    * Fixing odd merge

--- panoptes/piaa/utils/sources.py
+++ panoptes/piaa/utils/sources.py
@@ -141,13 +141,13 @@ def lookup_point_sources(fits_file,
 
         # Change the index to the picid
         point_sources.set_index('picid', inplace=True)
-        
+
         _print(f'Point sources: {len(point_sources)}')
 
         # Remove catalog matches that are too large
         _print(f'Removing matches that are greater than {max_catalog_separation} arcsec from catalog.')
         point_sources = point_sources.loc[point_sources.catalog_sep_arcsec < max_catalog_separation]
-        
+
     _print(f'Point sources: {len(point_sources)} {fits_file}')
 
     return point_sources
@@ -217,7 +217,6 @@ def get_catalog_match(point_sources, wcs, table='full_catalog', verbose=False, *
     return point_sources
 
 
-
 def _lookup_via_sextractor(fits_file, sextractor_params=None, trim_size=10, verbose=False, *args, **kwargs):
 
     def _print(msg):
@@ -271,7 +270,7 @@ def _lookup_via_sextractor(fits_file, sextractor_params=None, trim_size=10, verb
             raise Exception("Problem running sextractor: {}".format(e))
 
     # Read catalog
-    _print('Building detected source table with {source_file}')
+    _print(f'Building detected source table with {source_file}')
     point_sources = Table.read(source_file, format='ascii.sextractor')
 
     # Remove the point sources that sextractor has flagged

95a1ff490560e8c8ddfebbdfb6d95f0a43741216
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-02-18 19:26:34 +1100

    Removing old files

--- panoptes/piaa/tests/test_pipeline.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from panoptes.piaa import pipeline
-
-
-def test_get_imag():
-    assert pipeline.get_imag(10) == -2.5
--- panoptes/piaa/version.py
+++
@@ -1,6 +0,0 @@
-# this file was automatically generated
-major = 0
-minor = 0
-release = 3
-
-__version__ = '{major}.{minor}.{release}'

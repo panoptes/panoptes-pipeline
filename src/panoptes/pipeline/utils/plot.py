df02f52c8390053cd10ef38b33a268c8319c4c3f
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-03-14 10:30:05 -1000

    * Dumping plot updates.

--- src/panoptes/pipeline/utils/plot.py
+++ src/panoptes/pipeline/utils/plot.py
@@ -1,9 +1,48 @@
+import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 from astropy.visualization import simple_norm
 from panoptes.utils.images import bayer
 from panoptes.utils.images import plot as plot_utils
+
+
+def plot_background(rgb_bg_data, title=None):
+    """ Plot the RGB backgrounds from `Background2d` objects.
+
+    Args:
+        rgb_bg_data (list[photutils.Background2D]): The RGB background data as
+            returned by calling `panoptes.utils.images.bayer.get_rgb_background`
+            with `return_separate=True`.
+        title (str): The title for the plot, default None.
+
+    """
+
+    nrows = 2
+    ncols = 3
+
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
+    fig.set_facecolor('white')
+
+    for color in bayer.RGB:
+        d0 = rgb_bg_data[color]
+        ax0 = axes[0][color]
+        ax1 = axes[1][color]
+
+        ax0.set_title(f'{color.name.title()} (med {d0.background_median:.02f} ADU)')
+        im = ax0.imshow(d0.background, cmap=f'{color.name.title()}s_r', origin='lower')
+        plot_utils.add_colorbar(im)
+
+        ax1.set_title(f'{color.name.title()} rms (med {d0.background_rms_median:.02f} ADU)')
+        im = ax1.imshow(d0.background_rms, cmap=f'{color.name.title()}s_r', origin='lower')
+        plot_utils.add_colorbar(im)
+
+        ax0.set_axis_off()
+        ax1.set_axis_off()
+
+    if title:
+        fig.suptitle(title)
+
+    fig.set_size_inches(11, 5)
+    return fig
 
 
 def plot_stamp(picid,
@@ -14,31 +53,25 @@ def plot_stamp(picid,
                show_all=False,
                cmap=None,
                stretch='sqrt',
+               title=None,
                ):
-    cmap = cmap or plot_utils.get_palette('viridis')
+    cmap = cmap or plot_utils.get_palette()
 
-    picid = metadata.picid.iloc[0]
-
-    fig = Figure()
-    FigureCanvas(fig)
-    fig.set_figheight(4)
-    fig.set_figwidth(4)
-
-    nrows = 1
-    ncols = 1
-    ax = fig.add_subplot(nrows, ncols, 1)
+    fig, ax = plt.subplots()
 
     if frame_idx:
-        stamp = data.iloc[frame_idx]
+        # NOTE: this will fail if not square
+        stamp_size = int(np.sqrt(data.shape[-1]))
+        stamp = data.iloc[frame_idx].to_numpy().reshape(stamp_size, stamp_size)
     else:
         stamp = data
 
     # Get the frame bounds on full image.
-    y0, y1, x0, x1 = metadata.filter(regex='stamp').iloc[frame_idx]
+    y0, y1, x0, x1 = metadata.filter(regex='stamp').iloc[frame_idx or 0]
 
     # Get peak location on stamp.
-    x_peak = metadata.catalog_wcs_x_int.iloc[frame_idx] - x0
-    y_peak = metadata.catalog_wcs_y_int.iloc[frame_idx] - y0
+    x_peak = metadata.catalog_wcs_x_int.iloc[frame_idx or 0] - x0
+    y_peak = metadata.catalog_wcs_y_int.iloc[frame_idx or 0] - y0
 
     # Plot stamp.
     norm = simple_norm(stamp, stretch)
@@ -78,7 +111,13 @@ def plot_stamp(picid,
 
     ax.legend(loc=3)
 
-    ax.set_title(f'PICID: {picid} Frame: {frame_idx} / {len(metadata)}')
+    if frame_idx is None:
+        frame_idx = ''
+
+    title = title or f'PICID: {picid} Frame: {frame_idx} / {len(metadata)}'
+
+    ax.set_title(title)
+    fig.set_size_inches(14, 6)
     return fig
 
 

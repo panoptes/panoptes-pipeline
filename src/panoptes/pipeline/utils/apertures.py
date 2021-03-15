55cd73ab02df2c45d9673c5b1993332d5a6b6f04
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-03-15 12:29:17 -1000

    * Docstrings for location aperture util

--- src/panoptes/pipeline/utils/apertures.py
+++ src/panoptes/pipeline/utils/apertures.py
@@ -1,8 +1,12 @@
+from typing import Tuple
+
 import numpy as np
+import numpy.typing as npt
 from scipy import ndimage
 from astropy.stats import sigma_clip
 
 from panoptes.utils.images import bayer
+from panoptes.utils.logging import logger
 
 
 def get_rectangle_aperture(stamp_size, annulus_width=2):
@@ -47,22 +51,40 @@ def get_sigma_clip_aperture(data, sigma=2):
     return aperture_mask
 
 
-def make_location_aperture(dataframe, stamp_size, dilation_iterations=1):
-    aperture_mask = np.zeros(stamp_size)
+def make_location_aperture(x: npt.ArrayLike,
+                           y: npt.ArrayLike,
+                           aperture_size: Tuple[int, int] = (10, 10),
+                           dilation_iterations: int = 1,
+                           ):
+    """Make dilated apertures from the given x and y coordinate locations.
 
-    x_locs = (dataframe.catalog_wcs_x_int - dataframe.stamp_x_min).values
-    y_locs = (dataframe.catalog_wcs_y_int - dataframe.stamp_y_min).values
+    The `x` and `y` arrays can come from any source. Using PIPELINE metadata the
+    mean WCS catalog positions can be used like:
 
-    for loc in list(zip(y_locs, x_locs)):
+        x_locs = (dataframe.catalog_wcs_x_int - dataframe.stamp_x_min).values
+        y_locs = (dataframe.catalog_wcs_y_int - dataframe.stamp_y_min).values
+        make_location_aperture(x_locs, y_locs, (10, 10))
+
+    Args:
+        x (npt.ArrayLike): The x coordinate locations to dilate.
+        y (npt.ArrayLike): The y coordinate locations to dilate.
+        aperture_size (Tuple[int, int]): The initial size of the aperture, default (10, 10).
+        dilation_iterations (int): The number of binary dilations, default 1.
+
+    Returns:
+        npt.ArrayLike: The aperture mask.
+    """
+    aperture_mask = np.zeros(aperture_size)
+
+    for loc in list(zip(y, x)):
         try:
             aperture_mask[loc] = 1
         except Exception as e:
-            print(e)
+            logger.warning(f"Bad coordinate location for mask at {x=},{y=} {e!r}")
 
     if dilation_iterations > 0:
         aperture_mask = ndimage.binary_dilation(aperture_mask, iterations=dilation_iterations)
 
     # Negate the mask
     aperture_mask = ~np.ma.make_mask(aperture_mask)
-
     return aperture_mask

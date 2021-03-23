918361bc1eef21ff3ab08f19476ba3db96b7b698
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-03-23 12:42:45 -1000

    * Docstring cleanup for storage utils.

--- src/panoptes/pipeline/utils/gcp/storage.py
+++ src/panoptes/pipeline/utils/gcp/storage.py
@@ -3,14 +3,14 @@ from google.cloud.storage import Bucket
 
 def move_blob_to_bucket(blob_name: str, old_bucket: Bucket, new_bucket: Bucket,
                         remove: bool = True):
-    """Copy the blob from the incoming bucket to the `new_bucket`.
+    """Copy and optionally remove the blob from old to new bucket.
 
     Args:
         blob_name (str): The relative path to the blob.
         old_bucket (Bucket): The name of the bucket where we get the file.
         new_bucket (Bucket): The name of the bucket where we move/copy the file.
-        remove (bool, optional): If file should be removed afterwards, i.e. a move, or just copied.
-            Default True as per the function name.
+        remove (bool, optional): If True (the default), file should be removed
+            afterwards.
     """
     print(f'Moving {blob_name} → {new_bucket}')
     old_bucket.copy_blob(old_bucket.get_blob(blob_name), new_bucket)
@@ -19,5 +19,6 @@ def move_blob_to_bucket(blob_name: str, old_bucket: Bucket, new_bucket: Bucket,
 
 
 def copy_blob_to_bucket(*args, **kwargs):
+    """A thin-wrapper around `move_blob_to_bucket` that sets `remove=False`."""
     kwargs['remove'] = False
     move_blob_to_bucket(*args, **kwargs)

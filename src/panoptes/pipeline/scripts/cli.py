4b866de369b8730c6c357b95d1499f21fca94264
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-09-12 14:10:43 -1000

    * Add observation to cli.

--- src/panoptes/pipeline/scripts/cli.py
+++ src/panoptes/pipeline/scripts/cli.py
@@ -1,9 +1,11 @@
 import typer
 
 from panoptes.pipeline.scripts.image import app as image_app
+from panoptes.pipeline.scripts.observation import app as observation_app
 
 app = typer.Typer()
 app.add_typer(image_app, name="image")
+app.add_typer(observation_app, name="observation")
 
 if __name__ == "__main__":
     app()
